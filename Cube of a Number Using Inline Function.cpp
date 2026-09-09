9c744842a3b065651bb3f6b1b7ff6790d04e7142
mayank022546 <mayankpanse141@gmail.com>
2026-09-09 20:16:56 +0530

    5.1

---
+++ Cube of a Number Using Inline Function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+inline int cube(int n) {
+    return n * n * n;
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Cube of " << num << " = " << cube(num) << endl;
+    return 0;
+}
