b26aa0a4603e3bceb53d537ed83a61b7ee2cfff4
mayank022546 <mayankpanse141@gmail.com>
2026-09-09 20:04:00 +0530

    3.1

---
+++ Implicit Type Casting between Integer and Double.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    double b = 3.5;
+    double result = a + b;
+    cout << "int a = " << a << endl;
+    cout << "double b = " << b << endl;
+    cout << "Result (a + b) = " << result << endl;
+    cout << "double b assigned to int c = " << c << endl;
+    return 0;
+}
